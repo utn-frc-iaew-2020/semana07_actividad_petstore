9adfcb6246ce9a2c2896f7816aa8dfc9f99b3855
Ruben Romero <romeroa.ruben@gmail.com>
2020-04-28 15:57:22 -0300

    Solucion

--- APIPetstore/Controllers/PetsController.cs
+++ APIPetstore/Controllers/PetsController.cs
@@ -4,48 +4,126 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.Extensions.Configuration;
 
 namespace Petstore.Controllers
 {
+    [ApiVersion("1.0")]
     [ApiController]
-    [Route("[controller]")]
+    [Route("v{v:apiVersion}/[controller]")]
     public class PetsController : ControllerBase
     {
         private static List<Pets> listPets;
         private readonly ILogger<PetsController> _logger;
 
-        public PetsController(ILogger<PetsController> logger)
+        private readonly IConfiguration _config;
+
+        public PetsController(ILogger<PetsController> logger, IConfiguration config)
         {
             _logger = logger;
-            if(listPets == null)
+            _config = config;
+            if (listPets == null)
             {
                 listPets = new List<Pets>();
-                listPets.Add(new Pets() { 
-                    id = "123", 
-                    name = "collar perro",
-                    tag = "perro",
+                listPets.Add(new Pets()
+                {
+                    Id = "123",
+                    Name = "collar perro",
+                    Tag = "perro",
                 });
-                listPets.Add(new Pets() { 
-                    id = "124", 
-                    name = "collar gato",
-                    tag = "gato",
+                listPets.Add(new Pets()
+                {
+                    Id = "124",
+                    Name = "collar gato",
+                    Tag = "gato",
                 });
             }
-           
         }
 
         [HttpGet]
-        public IEnumerable<Pets> Get()
+        public ActionResult<IEnumerable<Pets>> GetAll(string name)
         {
-            return listPets;
+            _logger.LogInformation("Iniciando GetAll");
+            if (!string.IsNullOrEmpty(name))
+            {
+                var result = listPets.FindAll(p => p.Name.Contains(name));
+                if (result.Count == 0)
+                {
+                    return NotFound(result);
+                }
+                return Ok(result);
+            }
+            else
+            {
+                return Ok(listPets);
+            }
+
         }
 
         [HttpPost]
-        public Pets Post(Pets bodyParam)
+        public IActionResult Post(Pets bodyParam)
         {
+            _logger.LogInformation("Iniciando Post");
             listPets.Add(bodyParam);
 
-            return bodyParam;
+            return Created(string.Concat("/pets/", bodyParam.Id), bodyParam);
+        }
+
+        [HttpGet("{petId}")]
+        public IActionResult GetById(string petId)
+        {
+            _logger.LogInformation("Iniciando GetById");
+            if (!listPets.Exists(p => p.Id == petId))
+            {
+                return NotFound();
+            }
+            return Ok(listPets.FirstOrDefault(p => p.Id == petId));
+        }
+
+        [HttpPut("{petId}")]
+        public IActionResult Put(string petId, [FromBody] Pets body)
+        {
+            _logger.LogInformation("Iniciando Put");
+
+            if (!listPets.Exists(p => p.Id == petId))
+            {
+                return NotFound();
+            }
+
+            Pets item = listPets.FirstOrDefault(p => p.Id == petId);
+            item.Name = body.Name;
+            item.Tag = body.Tag;
+
+            return Ok(body);
+        }
+
+        [HttpPatch("{petId}")]
+        public IActionResult Patch(string petId, [FromBody] JsonPatchDocument<Pets> body)
+        {
+            _logger.LogInformation("Iniciando Patch");
+
+            if (!listPets.Exists(p => p.Id == petId))
+            {
+                return NotFound();
+            }
+            var item = listPets.FirstOrDefault(p => p.Id == petId);
+            body.ApplyTo(item);
+
+            return Ok(item);
+        }
+
+        [HttpDelete("{petId}")]
+        public IActionResult Delete(string petId)
+        {
+            _logger.LogInformation("Iniciando Delete");
+
+            if (!listPets.Exists(p => p.Id == petId))
+            {
+                return NotFound();
+            }
+            listPets.RemoveAll(p => p.Id == petId);
+            return NoContent();
         }
     }
 }
--- APIPetstore/Startup.cs
+++ APIPetstore/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc.Versioning;
 
 namespace Petstore
 {
@@ -25,7 +26,9 @@ namespace Petstore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers()
+            .AddNewtonsoftJson();
+            services.AddApiVersioning();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,6 +49,7 @@ namespace Petstore
             {
                 endpoints.MapControllers();
             });
+            
         }
     }
 }
