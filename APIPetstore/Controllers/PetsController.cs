9279345e99fd2445f61b5e1bcf070fcbc1a08465
Ruben Romero <romeroa.ruben@gmail.com>
2020-05-21 21:22:59 -0300

    Modificaciones Controller

--- APIPetstore/Controllers/PetsController.cs
+++ APIPetstore/Controllers/PetsController.cs
@@ -6,6 +6,12 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.Extensions.Configuration;
+using Amazon;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2.DocumentModel;
+using Amazon.DynamoDBv2.Model;
+using Amazon.Runtime;
 
 namespace Petstore.Controllers
 {
@@ -15,114 +21,106 @@ namespace Petstore.Controllers
     public class PetsController : ControllerBase
     {
         private static List<Pets> listPets;
+
         private readonly ILogger<PetsController> _logger;
 
         private readonly IConfiguration _config;
+
+        private readonly DynamoDBContext _context;
+
 
         public PetsController(ILogger<PetsController> logger, IConfiguration config)
         {
             _logger = logger;
             _config = config;
-            if (listPets == null)
-            {
-                listPets = new List<Pets>();
-                listPets.Add(new Pets()
-                {
-                    Id = "123",
-                    Name = "collar perro",
-                    Tag = "perro",
-                });
-                listPets.Add(new Pets()
-                {
-                    Id = "124",
-                    Name = "collar gato",
-                    Tag = "gato",
-                });
-            }
+            var client = new AmazonDynamoDBClient(RegionEndpoint.USEast1);
+
+            _context = new DynamoDBContext(client);
         }
 
         [HttpGet]
-        public ActionResult<IEnumerable<Pets>> GetAll(string name)
+        public async Task<ActionResult<IEnumerable<Pets>>> GetAll(string name)
         {
             _logger.LogInformation("Iniciando GetAll");
+
+            var conditions = new List<ScanCondition>();
             if (!string.IsNullOrEmpty(name))
             {
-                var result = listPets.FindAll(p => p.Name.Contains(name));
-                if (result.Count == 0)
-                {
-                    return NotFound(result);
-                }
-                return Ok(result);
+                conditions.Add(new ScanCondition("Name", ScanOperator.Equal, name));
+
             }
-            else
-            {
-                return Ok(listPets);
-            }
+
+            var listPets = await _context.ScanAsync<Pets>(conditions).GetRemainingAsync();
+            return Ok(listPets);
 
         }
 
         [HttpPost]
-        public IActionResult Post(Pets bodyParam)
+        public async Task<IActionResult> Post(Pets bodyParam)
         {
             _logger.LogInformation("Iniciando Post");
-            listPets.Add(bodyParam);
+
+            await _context.SaveAsync<Pets>(bodyParam);
 
             return Created(string.Concat("/pets/", bodyParam.Id), bodyParam);
         }
 
         [HttpGet("{petId}")]
-        public IActionResult GetById(string petId)
+        public async Task<IActionResult> GetById(string petId)
         {
             _logger.LogInformation("Iniciando GetById");
-            if (!listPets.Exists(p => p.Id == petId))
-            {
-                return NotFound();
-            }
-            return Ok(listPets.FirstOrDefault(p => p.Id == petId));
+            Pets item = await _context.LoadAsync<Pets>(petId);
+            return Ok(item);
         }
 
         [HttpPut("{petId}")]
-        public IActionResult Put(string petId, [FromBody] Pets body)
+        public async Task<IActionResult> Put(string petId, [FromBody] Pets body)
         {
             _logger.LogInformation("Iniciando Put");
+            Pets itemChanged = await _context.LoadAsync<Pets>(petId);
 
-            if (!listPets.Exists(p => p.Id == petId))
+            if (itemChanged == null)
             {
                 return NotFound();
             }
 
-            Pets item = listPets.FirstOrDefault(p => p.Id == petId);
-            item.Name = body.Name;
-            item.Tag = body.Tag;
+            itemChanged.Name = body.Name;
+            itemChanged.Tag = body.Tag;
 
+            await _context.SaveAsync<Pets>(itemChanged);
             return Ok(body);
         }
 
         [HttpPatch("{petId}")]
-        public IActionResult Patch(string petId, [FromBody] JsonPatchDocument<Pets> body)
+        public async Task<IActionResult> Patch(string petId, [FromBody] JsonPatchDocument<Pets> body)
         {
             _logger.LogInformation("Iniciando Patch");
 
-            if (!listPets.Exists(p => p.Id == petId))
+            Pets item = await _context.LoadAsync<Pets>(petId);
+
+            if (item == null)
             {
                 return NotFound();
             }
-            var item = listPets.FirstOrDefault(p => p.Id == petId);
+
             body.ApplyTo(item);
+            await _context.SaveAsync<Pets>(item);
 
             return Ok(item);
         }
 
         [HttpDelete("{petId}")]
-        public IActionResult Delete(string petId)
+        public async Task<IActionResult> Delete(string petId)
         {
             _logger.LogInformation("Iniciando Delete");
 
-            if (!listPets.Exists(p => p.Id == petId))
+            Pets item = await _context.LoadAsync<Pets>(petId);
+
+            if (item == null)
             {
                 return NotFound();
             }
-            listPets.RemoveAll(p => p.Id == petId);
+            await _context.DeleteAsync<Pets>(petId);
             return NoContent();
         }
     }
